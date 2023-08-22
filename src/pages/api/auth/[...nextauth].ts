b33c2ae0681b0075776ef8a64bf7abc6648c648f
Angelo Esmeralda <angeloje@macbookangelo.local>
2023-08-22 15:23:21 -0700

    update

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,14 +2,11 @@ import NextAuth from "next-auth";
 // import { authOptions } from "~/server/auth";
 import DiscordProvider from "next-auth/providers/discord";
 
-const id = (process.env.DISCORD_CLIENT_ID as string) || null;
-const secret = (process.env.DISCORD_CLIENT_SECRET as string) || null;
-
 export const authOptions = {
   providers: [
     DiscordProvider({
-      clientId: id!,
-      clientSecret: secret!,
+      clientId: process.env.DISCORD_CLIENT_ID! as string,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET! as string,
     }),
   ],
 };
