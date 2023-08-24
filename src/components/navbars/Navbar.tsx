f3e6a5aa2db3dc85111d3d130f3c3fcd40c3408b
Angelo Esmeralda <angeloje97@outlook.com>
2023-08-23 19:35:56 -0700

    Changed where the nav bar is

--- src/components/navbars/Navbar.tsx
+++ src/components/navbars/Navbar.tsx
@@ -1,7 +1,30 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+
 interface NavbarProperties {
   className: string;
 }
 
 const Navbar = () => {
-  return <></>;
+  return (
+    <>
+      <div>
+        <SignInOutButton />
+      </div>
+    </>
+  );
 };
+
+const SignInOutButton = () => {
+  const { data: sessionData } = useSession();
+
+  const label = !sessionData ? "Sign In" : "Sign Out";
+  const func = !sessionData ? () => void signIn() : () => void signOut();
+
+  return (
+    <>
+      <button onClick={func}>{label}</button>
+    </>
+  );
+};
+
+export default Navbar;
